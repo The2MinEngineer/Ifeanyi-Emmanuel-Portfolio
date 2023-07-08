eba8ece05aaa18dae2fa280753b9662b081f1019
pheany <114763753+pheany@users.noreply.github.com>
2023-07-08 19:59:16 +0100

    build

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FaTwitter, FaLinkedinIn } from 'react-icons/fa';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <FaLinkedinIn />
+    </div>
+    <div>
+      <FaTwitter />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
