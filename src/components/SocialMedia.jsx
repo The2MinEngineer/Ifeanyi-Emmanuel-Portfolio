580d7c8af022b59d7565b68801b3a0ca31da3362
pheany <114763753+pheany@users.noreply.github.com>
2023-07-08 20:23:15 +0100

    download button and link to resume

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import { FaTwitter, FaLinkedinIn } from 'react-icons/fa';
+import { FaTwitter, FaLinkedinIn, FaMedium, FaDownload } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <FaLinkedinIn />
+      <a href="https://www.linkedin.com/in/ifeanyi-emmanuel/">
+        <FaLinkedinIn />
+      </a>
     </div>
     <div>
-      <FaTwitter />
+      <a href="https://twitter.com/The2MinEngineer">
+        <FaTwitter />
+      </a>
+    </div>
+    <div>
+      <a href="https://medium.com/@The2MinEngineer">
+        <FaMedium />
+      </a>
+    </div>
+    <div>
+      <a href="https://drive.google.com/file/d/1HY329XidR9qTLVPX5PqOXkBjnh96Fydi/view?usp=drive_link">
+        <FaDownload />
+      </a>
     </div>
   </div>
 );
